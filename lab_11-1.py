359fa2c0c28ecdc0390421bf738fe64ff225a054
Jan Salafia <p23jsalafia@fairfieldprep.org>
2023-01-30 09:01:45 -0500

    Lab 11-1 Submission

---
+++ lab_11-1.py
@@ -0,0 +1,8 @@
+#Author JS Lab 11-1 1/30/2023
+
+row = ({27:'Jan',0:'Preston', 34:'Sean', 3:'Jack', 8:'Brian', 30:'Will', 36:'Ethan'})          #Create values for the dictionary
+
+print(row)          #Print Dictionary
+
+row[27] = "Not Jan"     #Change value of key 27
+print(row)              #Print dictionary
