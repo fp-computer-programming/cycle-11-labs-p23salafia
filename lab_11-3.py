85f44b8e4137f26bd6a39267ea09179c26d482ce
Jan Salafia <p23jsalafia@fairfieldprep.org>
2023-02-07 12:05:12 -0500

    Lab 11-3, 11-3 Functions Submission

--- lab_11-3.py
+++ lab_11-3.py
@@ -1,7 +1,8 @@
 #Author JS Lab 11-3 2/1/2023
 
-from lab_11_3_functions import add_nums, add_numbers, double_stuff
+from lab_11_3_functions import add_nums, add_numbers, double_stuff, food_info
 
 print(add_nums([1,2,3,4]))                  #Find the next 3 numbers in a list and also return the user's input
 print(add_numbers())                        #Add 2 input #'s together
 print(double_stuff([1,2,3]))                #Double the integer inputs
+print(food_info(["potatoes", "salsa", "cherries", "banana", "apple"]))     #Find the Sixth letters in the foods, list words that are not foods, and short (less that 6 letter) foods
--- lab_11_3_functions.py
+++ lab_11_3_functions.py
@@ -1,7 +1,7 @@
 #Author JS Lab 11-3 2/1/2023
 
 #1                                                                                              #15+ Lines
-def add_foods(foods):
+def food_info(foods):
     sixth_letter = []
     not_foods = []
     short_foods = []
@@ -19,7 +19,7 @@ def add_foods(foods):
     print("Not Foods:", str(not_foods))
     print("Short Foods:", str(short_foods))
 
-#2                                                                                              #While Loop + Input
+#2                                                                                              #While Loop + Input + List
 def add_nums(numbers):
     while True:
         user_input = input("Enter a number: ")
@@ -33,7 +33,7 @@ def add_nums(numbers):
     print("passed list: " + str(numbers) + " user input: " + str(user_input))
     print(result)
 
-#3                                                                                              #Dictionary + Enumerate Loop
+#3                                                                                              #Dictionary + Enumerate
     def indexed_names(names_list):
         [str(index) + ": " + name for index, name in enumerate(names_list)]
     return
@@ -45,7 +45,7 @@ def add_numbers():
     sum = int(num1) + int(num2)
     return sum
 
-#5                                                                                              #For Loop
+#5                                                                                              #For Loop + List
 def double_stuff (stuff):
     for index, var in enumerate(stuff):
             stuff[index] = var*2
@@ -53,9 +53,87 @@ def double_stuff (stuff):
 
 #6                                                                                              #List
 def indexed_names (names):
-    for index, vars in enumerate(names):            #break each name into a list
+    for index, vars in enumerate(names):
         namex = [*vars] 
         #recombining the lists into a string
         namex.insert(0, str(index)+": ")
         names[index] = "".join(namex)
     return (names)
+
+#7                                                                                              #15+ lines + Built in functions
+def find_missing_number(sequence):
+    try:
+        numbers = set(map(int, sequence.split()))
+    except ValueError:
+        answer = 1
+    else:
+        answer = 0
+        if numbers:
+            top = max(numbers)
+            for n in range(1, top):
+                if n not in numbers:
+                    answer = n
+                    break
+    
+    return answer
+
+#8 
+def solution(number):                                       # 15+ lines
+    sum=0
+    if number%3==0:
+        nearest_three=number-3
+    else:
+        nearest_three=number-(number%3)
+    if number%5==0:
+        nearest_five=number-5
+    else:
+        nearest_five=number-(number%5)
+    for x in range(nearest_three, 0, -3):
+        sum+=x
+    for y in range(nearest_five, 0, -5):
+        if y%3==0:
+            continue
+        else:
+            sum+=y
+    return sum
+
+#9                                                         #15+ Lines
+def hamming_distance(a, b):
+    count = 0
+    bin_a = str(bin(a))
+    bin_b = str(bin(b))
+    list_a = [i for i in bin_a]
+    list_b = [i for i in bin_b]
+    while len(list_a) != len(list_b):
+        if len(list_a) > len(list_b):
+            list_b.insert(2, "0")
+        if len(list_b) > len(list_a):
+            list_a.insert(2, "0")
+    for i in range(2, len(list_a)):
+        if list_a[i] != list_b[i]:
+            count += 1
+    return count
+
+#10                                                         #15+ Lines + Enumerate + Build in functions
+def sum_of_integers_in_string(s):
+    result = 0
+    current = ""
+    for i,char in enumerate(s):
+        if i != len(s) - 1:
+            if char.isnumeric():
+                current += char
+            else:
+                if len(current) >= 1:
+                    result += int(current)
+                    current = ""
+        else:
+            if char.isnumeric():
+                if current:
+                    current += char
+                    result += int(current)
+                else:
+                    result += int(char)
+            else:
+                if current:
+                    result += int(current)
+    return result
